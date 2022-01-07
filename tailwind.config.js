6c8ae141d68e6ae2368f5c22752565c54962d566
Abhinav <abhinav.sattiraju@gmail.com>
2022-01-06 20:37:52 -0500

    initial navbar progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'xl_max': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg_max': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md_max': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm_max': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
