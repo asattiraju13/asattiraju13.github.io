5d10e07886b22e7a24cdabf53435dcdfd076829d
Abhinav <abhinav.sattiraju@gmail.com>
2022-01-07 18:44:21 -0500

    navbar w/out dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    screens: {
-      'xl_max': {'max': '1279px'},
-      // => @media (max-width: 1279px) { ... }
-
-      'lg_max': {'max': '1023px'},
-      // => @media (max-width: 1023px) { ... }
-
-      'md_max': {'max': '767px'},
-      // => @media (max-width: 767px) { ... }
-
-      'sm_max': {'max': '639px'},
-      // => @media (max-width: 639px) { ... }
-    }
+    extend: {
+      flexBasis: {
+        'eighth': '12.5%'
+      },
+      screens: {
+        'navbar':{'min': '900px'},
+        'navbar_max':{'max': '900px'},
+        'xl_max': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg_max': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md_max': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm_max': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      },
+      colors: {
+        'main-blue': '#2A7FFF',
+        'main-purple': '#9955FF'
+      },
+      height: {
+        'icon': '4vw'
+      }
+    },
   },
-  plugins: [],
+  plugins: []
 }
