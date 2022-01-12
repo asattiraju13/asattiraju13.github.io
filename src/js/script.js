80227f7636d7aec7ce8f61e35d9215a0f019f42d
Abhinav <abhinav.sattiraju@gmail.com>
2022-01-11 23:35:38 -0500

    dropdown progress

--- src/js/script.js
+++ src/js/script.js
@@ -8,15 +8,42 @@ window.addEventListener('scroll', function() {
     } else {
         navbar.style.top = '0px'
     }
-
     prev = scroll;
 });
 
+document.querySelector('.arrow-button').addEventListener('mouseout', function() {
+    document.querySelector('.arrow-button').classList.add('hovered')
+})
+
+function toggleDropdown(event) {
+    var dropdown = document.querySelector('.dropdown');
+    
+    dropdown.classList.toggle('off');
+    //animations
+    dropdown.classList.toggle('on');
+    
+    if (event.relatedTarget) {
+        if (event.relatedTarget.classList.contains('link')) {
+            location = event.relatedTarget.getAttribute('href');
+            if (event.relatedTarget.classList.contains('resume')) {
+
+            } else {
+                window.open(location,"_blank");
+            }
+            console.log(event.relatedTarget)
+        }
+    }
+}
+
 var button = document.querySelector('.menubutton')
 
-button.addEventListener('click', function() {
+button.addEventListener('click',toggleDropdown);
+button.addEventListener('blur', toggleDropdown);
+
+document.addEventListener('scroll', function() {
     var dropdown = document.querySelector('.dropdown');
-    dropdown.classList.toggle('off');
-    dropdown.classList.toggle('on');
+    dropdown.classList.remove('on');
+    dropdown.classList.add('off');
 });
+
 
